c42d327c41c993a9ec2c5b8b0055999a24bb1f1a
UTKARSH SAINI <44905511+utkarshsaini19@users.noreply.github.com>
2023-02-03 01:35:12 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -9,8 +9,8 @@ try {
     const transporter = nodemailer.createTransport({
         host:process.env.HOST,
         service: process.env.SERVICE,
-        port: 465,
-        secure: Boolean(process.env.SECURE),
+        port: 587,
+        secure: FALSE,
         auth:{
             user:process.env.USER,
             pass:process.env.PASS
